29eb17bb3360bb4c14aa99c813621e70b849044f
czyt <czytcn@gmail.com>
2026-01-07 17:59:50 +0800

    add rime-wanxiang-updater

---
+++ Formula/rime-wanxiang-updater.rb
@@ -0,0 +1,36 @@
+class RimeWanxiangUpdater < Formula
+  desc "Rime Wanxiang dictionary updater"
+  homepage "https://github.com/ca-x/rime-wanxiang-updater"
+  version "0.5.6"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ca-x/rime-wanxiang-updater/releases/download/v0.5.6/rime-wanxiang-updater-darwin-arm64"
+      sha256 "becfd3e425955f37ef6032536496dd7f02cf2103c4728ee2095647113bdc7fd2"
+
+      def install
+        bin.install "rime-wanxiang-updater-darwin-arm64" => "rime-wanxiang-updater"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ca-x/rime-wanxiang-updater/releases/download/v0.5.6/rime-wanxiang-updater-darwin-amd64"
+      sha256 "0bc01751f1fb63a500c34db16e8d8fc036fbffa533a316a9f1ff4e43bce88cd0"
+
+      def install
+        bin.install "rime-wanxiang-updater-darwin-amd64" => "rime-wanxiang-updater"
+      end
+    end
+  end
+
+  def caveats
+    <<~EOS
+      rime-wanxiang-updater has been installed!
+      Run 'rime-wanxiang-updater --help' to get started.
+    EOS
+  end
+
+  test do
+    system "#{bin}/rime-wanxiang-updater", "--version"
+  end
+end
